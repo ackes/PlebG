3c53200bdb735141355878e36004a6b0d9581fea
ackes <33783181+ackes@users.noreply.github.com>
2017-12-04 13:26:19 +0100

    Erstellung Bitmap und DC
    Habe DC und Bitmap erstellt und Bitmap an DC angepasst.
    Fehlerbereinigung steht noch aus.

--- Projektmappe_PlebG/PlebG/plebg.cpp
+++ Projektmappe_PlebG/PlebG/plebg.cpp
@@ -1,7 +1,39 @@
-//test test
 #include <iostream>
 #include <Windows.h>
 #include <stdio.h>
+
+//Jedesmal wenn kopiert wird, wird eine neue Bitmap erstellt und an unser DC angepasst -- unperformant, aber leichter zu verstehen
+void CopyBITMAPToDC(int DCwidth, int DCheight, int X, int Y, RECT GameRECT, HDC DeviceContext) {
+	//Der BITMAPInfoHeader für die Rahmenbedingungen der Bitmap
+	tagBITMAPINFOHEADER BitmapInfoHeader;
+	//Bitmapinfo für die Farbgebung etc.
+	tagBITMAPINFO BitmapInfo;
+	BitmapInfo.bmiHeader = BitmapInfoHeader;
+	
+	BitmapInfoHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+	BitmapInfoHeader.biPlanes = 1;
+	BitmapInfoHeader.biBitCount = 32;
+	BitmapInfoHeader.biCompression = BI_RGB;
+	BitmapInfoHeader.biSizeImage;
+	BitmapInfoHeader.biClrUsed = 0; // 0 bedeutet, dass die Bitmap die maximale Anzahl an möglichen Farben nutzt
+	BitmapInfoHeader.biClrImportant = 0;
+	int BMwidth = BitmapInfoHeader.biWidth;
+	int BMheight = -BitmapInfoHeader.biHeight;
+
+	//BitmapMemory muss über die VirtualAlloc Funktion zugewiesen werden. Die Größe dieses Speichers entspricht der Höhe*Breite*Anzahl der Bytes der Bitmap
+	int BitmapMemorySize = BMwidth*BMheight * 4;
+	void *BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+
+	//StretchDIBits um Bitmap auf DeviceContext zu kopieren
+	StretchDIBits(
+						DeviceContext,
+						0, 0, DCwidth, DCheight,
+						0, 0, BMwidth, BMheight,
+						BitmapMemory,
+						&BitmapInfo,
+						DIB_RGB_COLORS,
+						SRCCOPY);	
+}
 
 LRESULT CALLBACK WndProc(
 	HWND hwnd,
@@ -64,17 +96,26 @@ int CALLBACK WinMain(
 			Instance,
 			0);
 
-    	//if(GameWindow) {
-    	//	ShowWindow(GameWindow, SW_SHOW);
-    	//	UpdateWindow(GameWindow);
-    	//}
+			
 
-    	MSG msg;
-    	while (GetMessage(&msg, NULL, 0, 0)) {
-    		TranslateMessage(&msg);
-    		DispatchMessage(&msg);
-    	} return (int) msg.wParam;
+			//if(GameWindow) {
+			//	ShowWindow(GameWindow, SW_SHOW);
+			//	UpdateWindow(GameWindow);
+			//}
 
+			MSG msg;
+			while (GetMessage(&msg, NULL, 0, 0)) {
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			} return (int)msg.wParam;
+
+			HDC DeviceContext = GetDC(GameWindow);
+			RECT GameRECT;
+			int DCwidth = GameRECT.right - GameRECT.left;
+			int DCheight = GameRECT.bottom - GameRECT.top;
+			GetClientRect(GameWindow, &GameRECT);
+			CopyBITMAPToDC(DCwidth, DCheight, 0, 0, GameRECT, DeviceContext);
+			ReleaseDC(GameWindow, DeviceContext);
     }
     else {
     	MessageBox(
