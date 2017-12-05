0524b5079138bd9b4e509bef364af7418619a9eb
ackes <33783181+ackes@users.noreply.github.com>
2017-12-05 17:27:15 +0100

    Nichts neues, nur mehr Verständnis/Struktur reingebracht
    Habe unsere alles in einem Funktion doch gesplittet, Verständnis zum "malen" ist noch nicht 100% da.

--- Projektmappe_PlebG/PlebG/plebg.cpp
+++ Projektmappe_PlebG/PlebG/plebg.cpp
@@ -1,43 +1,79 @@
+//TODO:
+//Programm funktioniert nicht -- Vielleicht Denkfehler: Bitmap ist das was man auf dem Bildschirm sieht und der Buffer ist an einer anderen Stelle (obwohl Bitmapmemory Bitmapmemory heißt)
+//Prüfen, ob Src/Dest bei StretchDIBits korrekt eingetragen sind und ob auch die Src mit "Farbe" gefüllt wird über unsere DrawingFunction
+//DrawingFunction muss noch entsprechend korrigiert werden (Speicher Überlauf) und an korrekte Stellen eingetragen werden)
+
 #include <iostream>
 #include <Windows.h>
 #include <stdio.h>
 
-//NOTE(Patrick): Jedesmal wenn kopiert wird, wird eine neue Bitmap erstellt und an unser DC angepasst -- unperformant, aber leichter zu verstehen
-void CopyBITMAPToDC(int DCwidth, int DCheight, int X, int Y, RECT GameRECT, HDC DeviceContext) {
-	
-	//TODO(Patrick): VirtualFree den angelegten Speicher - Funktion in zwei Funktionen splitten
-	
-	//NOTE(Patrick): Bitmapinfo für die Farbgebung etc
-	int BMwidth = DCwidth;
-	int BMheight = DCheight;
+static bool Running;
+static int BMwidth;
+static int BMheight;
+static void *BitmapMemory;
+static BITMAPINFO BitmapInfo;
 
-	BITMAPINFO BitmapInfo;
-	BitmapInfo.bmiHeader.biWidth;
-	BitmapInfo.bmiHeader.biHeight;
+void DrawingFunction()
+{
+	unsigned int jump = BMwidth * 4;
+	unsigned char *Row = (unsigned char*)BitmapMemory; //NOTE(Patrick): Cast von Void auf Char (8 Bit Integer => 0-255 wegen unsigned)
+
+	for (int x = 0; x < BMheight; x++) {
+		unsigned char *Pixel = (unsigned char*)Row;
+		for (int y = 0; y < BMwidth; y++) {
+			*Pixel = 0;
+			++Pixel;
+			*Pixel = 0;
+			++Pixel;
+			*Pixel = 255;
+			++Pixel;
+			*Pixel = 0;
+			++Pixel;
+		}
+		Row = Row + jump;
+	}
+}
+
+
+void CreateBitmap(int width, int height) {
+	//TODO(Patrick): VirtualFree den angelegten Speicher
+	if (BitmapMemory)
+	{
+		VirtualFree(BitmapMemory, 0, MEM_RELEASE);
+	}
+
+	//NOTE(Patrick): Bitmapinfo für die Farbgebung etc
+	BMwidth = width;
+	BMheight = height;
+
+	BitmapInfo.bmiHeader.biWidth = BMwidth;
+	BitmapInfo.bmiHeader.biHeight = -BMheight;
 	BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
 	BitmapInfo.bmiHeader.biPlanes = 1;
 	BitmapInfo.bmiHeader.biBitCount = 32;
 	BitmapInfo.bmiHeader.biCompression = BI_RGB;
-	BitmapInfo.bmiHeader.biSizeImage;
-	BitmapInfo.bmiHeader.biClrUsed = 0;
-	BitmapInfo.bmiHeader.biClrImportant = 0;
-	BitmapInfo.bmiHeader.biWidth = BMwidth;
-	BitmapInfo.bmiHeader.biHeight = -BMheight;
+	
 
 	//NOTE(Patrick): BitmapMemory muss über die VirtualAlloc Funktion zugewiesen werden. Die Größe dieses Speichers entspricht der Höhe*Breite*Anzahl der Bytes der Bitmap
 	int BitmapMemorySize = BMwidth*BMheight * 4;
 	void *BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
-
-	//Note(Patrick): StretchDIBits um Bitmap auf DeviceContext zu kopieren
-	StretchDIBits(
-						DeviceContext,
-						0, 0, DCwidth, DCheight,
-						0, 0, BMwidth, BMheight,
-						BitmapMemory,
-						&BitmapInfo,
-						DIB_RGB_COLORS,
-						SRCCOPY);	
 }
+
+void CopyBitmapToDC(HDC DeviceContext, RECT *GameRECT, int width, int height, int x, int y) {		//Pointer des RECTS um nicht den kompletten Stack mit einem Rectangle vollzumachen
+	int WWidth = GameRECT->right - GameRECT->left;
+	int WHeight = GameRECT->bottom - GameRECT->top;
+																										//Note(Patrick): StretchDIBits um DC Parameter auf Repaintregion (Bitmap) zu kopieren
+	StretchDIBits(
+		DeviceContext,
+		0, 0, WWidth, WHeight,
+		0, 0, BMwidth, BMheight,
+		BitmapMemory,
+		&BitmapInfo,
+		DIB_RGB_COLORS,
+		SRCCOPY);
+}
+
+
 
 LRESULT CALLBACK WndProc(
 	HWND hwnd,
@@ -48,24 +84,56 @@ LRESULT CALLBACK WndProc(
 	LRESULT Result = 0;
 
 	switch (uMsg) {
-	case WM_CREATE:
-		// Initialize the window. 
-		return 0;
+		case WM_CLOSE:
+		{
+			OutputDebugStringA("CLOSE\n");
+			Running = FALSE;
+		}break;
+		case WM_CREATE:
+		{
+			OutputDebugStringA("CREATE\n");
+			RECT GameRECT;											//Hier werden die Größen vom WindowsWindow übernommen und in die Bitmap geschrieben (Beim Start des Programms
+			GetClientRect(hwnd, &GameRECT);
+			int width = GameRECT.right - GameRECT.left;
+			int height = GameRECT.bottom - GameRECT.top;
+			CreateBitmap(width, height);
+		}break;
+		case WM_PAINT:
+		{
+			OutputDebugStringA("PAINT\n");
+			PAINTSTRUCT Paint;
+			HDC DeviceContext = BeginPaint(hwnd, &Paint);
+			
+			int x = Paint.rcPaint.left;
+			int y = Paint.rcPaint.top;
+			int Paintwidth = Paint.rcPaint.right - Paint.rcPaint.left;
+			int Paintheight = Paint.rcPaint.bottom - Paint.rcPaint.top;
 
-	case WM_PAINT:
-		// Paint the window's client area. 
-		return 0;
+			RECT GameRECT;
+			GetClientRect(hwnd, &GameRECT);
+			CopyBitmapToDC(DeviceContext, &GameRECT, Paintwidth, Paintheight, x, y);
+			//DrawingFunction();
+			EndPaint(hwnd, &Paint);
+			
+		}break;
+		case WM_SIZE:
+		{ 
+			OutputDebugStringA("SIZE\n");
+		}break;
+		case WM_DESTROY:
+		{
+			OutputDebugStringA("DESTROY\n");
+			if (BitmapMemory)
+			{
+				VirtualFree(BitmapMemory, 0, MEM_RELEASE);
+			}
 
-	case WM_SIZE:
-		// Set the size and position of the window. 
-		return 0;
-
-	case WM_DESTROY:
-		// Clean up window-specific data objects. 
-		return 0;
-
-	default:
-		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+			Running = FALSE;			
+		}break;
+		default: 
+		{
+			return DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}break;
 	}
 
 	return 0;
@@ -87,12 +155,12 @@ int CALLBACK WinMain(
     WNDClass.lpszClassName = "PlebG";
 
     if (RegisterClass(&WNDClass)) {
-    	HWND GameWindow = CreateWindowEx(
+    	HWND WindowsWindow = CreateWindowEx(
 			0,
   			//WNDClass.lpszClassName,
   			"PlebG",
   			"PlebG-WindowName",
-  			WS_VISIBLE,
+			WS_OVERLAPPEDWINDOW|WS_VISIBLE,
 			CW_USEDEFAULT,
 			CW_USEDEFAULT,
 			CW_USEDEFAULT,
@@ -101,28 +169,30 @@ int CALLBACK WinMain(
   			0,
 			Instance,
 			0);
-
+		Running = TRUE;
 			
-		
-
-		for (;;)				//NOTE(Patrick): Solange das GameWindow offen ist, müssen die Funktionen, nach jeder Nachricht neu ausgeführt werden
+		while(Running)				//NOTE(Patrick): Solange das GameWindow offen ist, müssen die Funktionen, nach jeder Nachricht neu ausgeführt werden
 		{
 			MSG msg;
 			//NOTE(Patrick): Unlike GetMessage, the PeekMessage function does not wait for a message to be posted before returning.
-			while (PeekMessage(&msg, GameWindow, 0, 0, PM_REMOVE))
+			while (PeekMessage(&msg, WindowsWindow, 0, 0, PM_REMOVE))
 			{
+				if (msg.message == WM_QUIT) {
+					Running = FALSE;
+				}
 				TranslateMessage(&msg);
 				DispatchMessage(&msg);
 			}
 
-			HDC DeviceContext = GetDC(GameWindow);
+
+			HDC DeviceContext = GetDC(WindowsWindow);
 			RECT GameRECT;
-			GetClientRect(GameWindow, &GameRECT);
-			int DCwidth = GameRECT.right - GameRECT.left;
-			int DCheight = GameRECT.bottom - GameRECT.top;
+			GetClientRect(WindowsWindow, &GameRECT);
+			int RECTwidth = GameRECT.right - GameRECT.left;
+			int RECTheight = GameRECT.bottom - GameRECT.top;
 			
-			CopyBITMAPToDC(DCwidth, DCheight, 0, 0, GameRECT, DeviceContext);
-			ReleaseDC(GameWindow, DeviceContext);
+			CopyBitmapToDC(DeviceContext, &GameRECT, RECTwidth, RECTheight, 0, 0);
+			ReleaseDC(WindowsWindow, DeviceContext);
 		}
     }
     else {
@@ -133,4 +203,5 @@ int CALLBACK WinMain(
 			NULL);
 		return 1;
     }
+	return 0;
 }
